63069d8aa673879f7b6fd51f17090e7c269dfc6a
Nandlal <shahnandlal49@gmail.com>
2023-02-03 18:13:10 +0530

    added

--- all_component/src/Components/Useref/Useref.js
+++ all_component/src/Components/Useref/Useref.js
@@ -1,12 +1,20 @@
 import React,{useRef} from 'react'
 
 function Useref() {
-  const position = useRef(null)
-  
+  const inputRef = useRef(null)
+  const handleClick=()=>{
+    inputRef.current.focus()
+    inputRef.current.value="100";
+    inputRef.current.style.color="red"
+
+  }
+
+
     return (
     <div>
-<input type="text" placeholder='Enter your Text here'  />
-<button>Click me</button>
+    <h1>Use Ref in react</h1>
+<input type="text" placeholder='Enter your Text here' ref={inputRef} />
+<button onClick={handleClick}>Click me</button>
 
 
     </div>
