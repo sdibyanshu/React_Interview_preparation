60ebb818435f46c724a9a46697df1c20c8dbc844
Nandlal <shahnandlal49@gmail.com>
2023-02-08 11:54:11 +0530

    added on useReducer app

--- all_component/src/App.js
+++ all_component/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import SimpleApp from "./Components/UseReducerSimpleApp/SimpleApp";
+
 
 function App() {
   return (
     <div className="App">
-       <SimpleApp/>
+       
     </div>
   )
 }
