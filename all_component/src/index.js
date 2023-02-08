71972b70ea42b53614f50678892c05f0fdd59bfc
Nandlal <shahnandlal49@gmail.com>
2023-02-08 11:47:40 +0530

    added on useReducer app

--- all_component/src/index.js
+++ all_component/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {AppContext} from './Components/UseReducerSimpleApp/AppContext'
+// import {AppContext} from './Components/UseReducerSimpleApp/AppContext'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppContext>
+   
       <App />
-    </AppContext>
+    
   </React.StrictMode>
 );
 
