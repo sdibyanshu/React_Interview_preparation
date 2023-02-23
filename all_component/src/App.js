cdda641df9950153b41734ad2e5f20260c4c346f
sdibyanshu <sdibyanshu@gmail.com>
2023-02-23 17:07:58 +0530

    added

--- all_component/src/App.js
+++ all_component/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import FetchPagination from "./Components/FetchPagination/FetchPagination";
-import St from "./Components/StopWatch/St";
+import EventHandle from "./Components/MiniCalculator/Event";
 
-import StopWatch from "./Components/StopWatch/StopWatch";
 
 
 function App() {
   return (
     <div className="App">
-     <FetchPagination/>
+    <EventHandle/>
     </div>
   )
 }
