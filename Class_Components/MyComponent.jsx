51608334f03e06937527246c4815cd92c7149e1b
sdibyanshu <sdibyanshu@gmail.com>
2023-03-16 11:35:53 +0530

    Class component example

---
+++ Class_Components/MyComponent.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My Component</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment Count</button>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
--- Upcommimg/New.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const New = () => {
-  return (
-    <div>New</div>
-  )
-}
-
-export default New
