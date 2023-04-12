a1f0d6d6063f35887760091adcdbe711fb8862ad
Nandlal <shahnandlal49@gmail.com>
2023-04-12 23:39:22 +0530

    adding new timer with single start and stop button

--- all_component/src/Components/SimpleTimerwithbuttons/Timer.js
+++ all_component/src/Components/SimpleTimerwithbuttons/Timer.js
@@ -1,39 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function Timer() {
-    const [count,setcount]=useState(0);
-    const [text,settext]=useState('Start');
-    const [running,setrunning]=useState(false);
-    const handleStart=()=>{
-      setrunning(true);
-      settext("STOP")
-      if(text==="STOP"){
-        setrunning(false);
-        settext("Start")
-      }
-    }
-    const handleresume=()=>{
-      setcount(0);
+  const [count, setcount] = useState(0);
+  const [text, settext] = useState("Start");
+  const [running, setrunning] = useState(false);
+  const handleStart = () => {
+    setrunning(true);
+    settext("STOP");
+    if (text === "STOP") {
       setrunning(false);
+      settext("Start");
     }
-   
-    useEffect(()=>{
-    let interval=null;
-     if(running){
-      interval=setInterval(() => {
-        setcount((prev)=>prev+1)
+  };
+  const handleresume = () => {
+    setcount(0);
+    setrunning(false);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (running) {
+      interval = setInterval(() => {
+        setcount((prev) => prev + 1);
       }, 1000);
-    }else if(!running){
+    } else if (!running) {
       clearInterval(interval);
     }
-      return ()=>clearInterval(interval);
-   },[running])
-  
-  
-  
-    return (
-    <div>A Simple Timer</div>
-  )
+    return () => clearInterval(interval);
+  }, [running]);
+
+  return (
+    <div>
+      A Simple Timer
+      <h1>{count}</h1>
+      <button onClick={handleStart}>{text}</button>
+      <button onClick={handleresume}>Reset</button>
+    </div>
+  );
 }
 
-export default Timer
+export default Timer;
